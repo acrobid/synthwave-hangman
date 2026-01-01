830911b6b2f349b6316bb278fb260c8a576fe0b0
acrobid <116527022+acrobid@users.noreply.github.com>
2026-01-01 21:12:51 +0900

    Revert "Merge pull request #1 from acrobid/copilot/redesign-user-interface"
    This reverts commit 9091871013268259600816bfe0b05187fd25817b, reversing
    changes made to e6818aac99fd5c90bcf392a84bba403e6df72a62.

--- src/components/HangmanDrawing.vue
+++ src/components/HangmanDrawing.vue
@@ -5,55 +5,47 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-4">
-    <p class="text-xl text-cyan-300">Incorrect Guesses: {{ incorrectGuesses }} / 6</p>
-    <div class="relative w-64 h-64">
-      <!-- Gallows Base -->
-      <div class="absolute bottom-0 left-0 w-full h-2 bg-pink-500"></div>
-      
-      <!-- Gallows Pole -->
-      <div class="absolute bottom-0 left-16 w-2 h-48 bg-pink-500"></div>
-      
-      <!-- Gallows Top Bar -->
-      <div class="absolute top-0 left-16 w-32 h-2 bg-pink-500"></div>
-      
-      <!-- Gallows Rope -->
-      <div class="absolute top-2 left-[11.5rem] w-2 h-12 bg-pink-500"></div>
+  <div class="grid place-items-center">
+    Incorrect Guesses: {{ incorrectGuesses }}
+    <div class="relative w-48 h-48">
+      <!-- Base -->
+      <div class="absolute bottom-0 w-full h-2 bg-pink-500"></div>
+      <!-- Pole -->
+      <div class="absolute bottom-0 left-1/4 w-2 h-full bg-pink-500"></div>
+      <!-- Top -->
+      <div class="absolute top-0 left-1/4 w-24 h-2 bg-pink-500"></div>
+      <!-- Rope -->
+      <div class="absolute top-0 right-1/4 w-2 h-12 bg-pink-500"></div>
 
       <!-- Head -->
       <div
         v-if="incorrectGuesses >= 1"
-        class="absolute top-14 left-[10.5rem] w-10 h-10 rounded-full border-4 border-cyan-400 animate-fade-in"
+        class="absolute top-12 right-1/4 transform -translate-x-1/2 w-8 h-8 rounded-full border-4 border-cyan-400 animate-fade-in"
       ></div>
-      
       <!-- Body -->
       <div
         v-if="incorrectGuesses >= 2"
-        class="absolute top-24 left-[11.75rem] w-2 h-16 bg-cyan-400 animate-fade-in"
+        class="absolute top-20 right-1/4 transform -translate-x-7 w-2 h-16 bg-cyan-400 animate-fade-in"
       ></div>
-      
       <!-- Left Arm -->
       <div
         v-if="incorrectGuesses >= 3"
-        class="absolute top-28 left-[10rem] w-12 h-2 bg-cyan-400 origin-right rotate-45 animate-fade-in"
+        class="absolute top-24 right-1/4 transform -translate-x-1/2 w-8 h-2 bg-cyan-400 rotate-45 origin-right animate-fade-in"
       ></div>
-      
       <!-- Right Arm -->
       <div
         v-if="incorrectGuesses >= 4"
-        class="absolute top-28 left-[12rem] w-12 h-2 bg-cyan-400 origin-left -rotate-45 animate-fade-in"
+        class="absolute top-24 right-1/4 transform -translate-x-1/2 w-8 h-2 bg-cyan-400 -rotate-45 origin-left animate-fade-in"
       ></div>
-      
       <!-- Left Leg -->
       <div
         v-if="incorrectGuesses >= 5"
-        class="absolute top-40 left-[10.25rem] w-10 h-2 bg-cyan-400 origin-right rotate-45 animate-fade-in"
+        class="absolute top-36 right-1/4 transform -translate-x-1/2 w-8 h-2 bg-cyan-400 rotate-45 origin-right animate-fade-in"
       ></div>
-      
       <!-- Right Leg -->
       <div
         v-if="incorrectGuesses >= 6"
-        class="absolute top-40 left-[12rem] w-10 h-2 bg-cyan-400 origin-left -rotate-45 animate-fade-in"
+        class="absolute top-36 right-1/4 transform -translate-x-1/2 w-8 h-2 bg-cyan-400 -rotate-45 origin-left animate-fade-in"
       ></div>
     </div>
   </div>
@@ -67,11 +59,11 @@ defineProps<{
 @keyframes fadeIn {
   from {
     opacity: 0;
-    transform: scale(0.8);
+    transform: scale(0.8) translateX(-50%);
   }
   to {
     opacity: 1;
-    transform: scale(1);
+    transform: scale(1) translateX(-50%);
   }
 }
 </style>
